c63d1335a943e02a462f8691240c8fb8449584e5
peng.zhang <peng.zhang@i-tudou.com>
2025-08-14 17:54:21 +0800

    bert-base-chinese模型下载

--- src/transformers_one/model_download.py
+++ src/transformers_one/model_download.py
@@ -1,19 +1,30 @@
 # 将模型下载到本地调用
 # AutoTokenizer 分词工具，词向量工具
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
 
 # 将模型和分词工具：下载到本地，并指定保存路劲
-model_name = "uer/gpt2-chinese-cluecorpussmall"
-cache_dir = "model/uer/gpt2-chinese-cluecorpussmall"
+# model_name = "uer/gpt2-chinese-cluecorpussmall"
+# cache_dir = "model/uer/gpt2-chinese-cluecorpussmall"
 
 # 下载模型
-AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir)
+# AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir)
 # 下载分词工具
-AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+# AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
 
-#总共800M+
-print(f"模型分词器已下载到：{cache_dir}")
+# 总共800M+
+# print(f"模型分词器已下载到：{cache_dir}")
 
-#模型是放在这里：E:\project\python\slm_model_train\src\transformers_one\model\uer\gpt2-chinese-cluecorpussmall\models--uer--gpt2-chinese-cluecorpussmall\snapshots
-#pytorch_model.bin 是模型，有401M，config是模型的配置文件，其中的"tokenizer_class": "BertTokenizer",说明了当前模型使用的分词器，"vocab_size": 21128 字符量
-#把所有的字，都放在vocab.txt中，将词放在文件中，进行位置编码，对应的字翻译成对应的位置数字
+# 模型是放在这里：E:\project\python\slm_model_train\src\transformers_one\model\uer\gpt2-chinese-cluecorpussmall\models--uer--gpt2-chinese-cluecorpussmall\snapshots
+# pytorch_model.bin 是模型，有401M，config是模型的配置文件，其中的"tokenizer_class": "BertTokenizer",说明了当前模型使用的分词器，"vocab_size": 21128 字符量
+# 把所有的字，都放在vocab.txt中，将词放在文件中，进行位置编码，对应的字翻译成对应的位置数字
+
+
+# 模型名称
+model_name = "bert-base-chinese"
+# 本地保存路径
+cache_dir = r"E:\project\python\slm_model_train\src\model_data"
+
+# 下载BERT模型
+model = AutoModel.from_pretrained(model_name, cache_dir=cache_dir)
+# 下载对应的分词工具
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
